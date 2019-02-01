22f0a81f83fc4d0d06a6b0687fa41eebf271f015
Александр Харченко <kharchenkoau31@gmail.com>
2019-02-01 10:15:40 +0200

    Initial Commit

---
+++ EvaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EvaTest
+//
+//  Created by Александр Харченко on 2/1/19.
+//  Copyright © 2019 Александр Харченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
